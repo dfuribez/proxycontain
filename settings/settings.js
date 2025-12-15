422e91bb4e02041fa7aa5665cfcb2560022af41e
Diego <duribe>
2025-12-14 22:06:42 -0500

    add initial settings

---
+++ settings/settings.js
@@ -0,0 +1,39 @@
+import * as utils from "../utils/utils.js";
+
+async function loadSettings() {
+  var settings = await browser.storage.local.get(null);
+
+  document.getElementById("proxy1-host").value = settings["proxy1-host"]
+    ? settings["proxy1-host"]
+    : "";
+  document.getElementById("proxy1-port").value = settings["proxy1-port"]
+    ? settings["proxy1-port"]
+    : 0;
+
+  document.getElementById("proxy2-host").value = settings["proxy2-host"]
+    ? settings["proxy2-host"]
+    : "";
+  document.getElementById("proxy2-port").value = settings["proxy2-port"]
+    ? settings["proxy2-port"]
+    : 0;
+}
+
+async function setClickable() {
+  document.getElementById("set-proxy-button").onclick = saveProxy;
+}
+
+async function saveProxy() {
+  browser.storage.local.set({
+    "proxy1-host": utils.sanitize(document.getElementById("proxy1-host").value),
+    "proxy1-port": utils.sanitize(document.getElementById("proxy1-port").value),
+    "proxy2-host": utils.sanitize(document.getElementById("proxy2-host").value),
+    "proxy2-port": utils.sanitize(document.getElementById("proxy2-port").value),
+  });
+}
+
+async function main() {
+  await loadSettings();
+  await setClickable();
+}
+
+document.addEventListener("DOMContentLoaded", main);
